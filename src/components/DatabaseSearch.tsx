7245d227180aedd054930c74d56db5f653273e35
George-E-M <georgeedwinmcerlean@gmail.com>
2018-11-21 16:57:15 +1300

    Set up components

---
+++ src/components/DatabaseSearch.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+
+interface IProps {
+    databaseList: any[],
+    recentlySearchedList: any[]
+}
+
+export default class DatabaseSearch extends React.Component<IProps, {}> {
+    constructor(props: any) {
+        super(props)
+    }
+
+    public render() {
+        return(
+            <div className="container search-wrapper">
+                <div className="search-heading">
+                    Search for the Word you want to practice
+                </div>
+                <div className="row search-btn-container">
+                    <div className="btn app-btn" onClick={this.viewDatabase}> View Database </div>
+                    <div className="btn app-btn" onClick={this.addToDatabase}> Add To Database </div>
+                </div>
+                <div className="search-container">
+                    <input type="text" id="search-textbox" className="search-bar" placeholder="Search For a Word" />
+                    <div className="search-container-divider">
+                        <div className="btn search-button" onClick = {this.searchForWord}>Search</div>
+                    </div>
+                </div>
+                <div className="recently-searched-table">
+                    <table className="table recent-searches">
+                        <tbody>
+                            {this.createTable()}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+
+    private viewDatabase() {
+        
+    }
+
+    private addToDatabase() {
+
+    }
+
+    private searchForWord() {
+
+    }
+
+    private createTable() {
+        const table:any[] = []
+        return table
+    }
+}
---
+++ src/components/RecordingDisplay.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+
+interface IProps {
+    databaseRecording: any,
+    personalRecording: any
+}
+
+interface IState {
+    open: boolean
+    personalExists: boolean
+}
+
+export default class RecordingDisplay extends React.Component<IProps, IState> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            open: false,
+            personalExists: false
+        }
+    }
+
+    public render() {
+        const databaseRecording = this.props.databaseRecording
+        return(
+            <div className="container recording-wrapper">
+                <div className="row recording-heading">
+                    <b>{databaseRecording.Word}</b>&nbsp; {databaseRecording.Tag}
+                </div>
+                <div className="row recording-date">
+                    {databaseRecording.Uploaded}
+                </div>
+                <div className="row recording-audio">
+                    <div className="btn app-btn" onClick={this.playDatabaseRecording}>Play</div>
+                </div>
+                <div className="row record-button-row">
+                    <div className="btn app-btn" onClick={this.recordAttempt}>Record</div>
+                </div>
+                <div className="row personal-recording-audio">
+                    <div className="btn app-btn" onClick={this.playPersonalRecording}>Play Your Attempt</div>
+                </div>
+
+                <div className="row recording-button-row">
+                    <div className="btn app-btn" onClick={this.editRecording}>Edit</div>
+                    <div className="btn app-btn" onClick={this.rateRecording}>Report</div>
+                    <div className="btn app-btn" onClick={this.deleteRecording}>Delete</div>
+                </div>
+            </div>
+        );
+    }
+
+    private playDatabaseRecording() {
+        
+    }
+
+    private recordAttempt() {
+
+    }
+
+    private playPersonalRecording() {
+
+    }
+
+    private editRecording() {
+
+    }
+
+    private rateRecording() {
+
+    }
+
+    private deleteRecording() {
+
+    }
+}
